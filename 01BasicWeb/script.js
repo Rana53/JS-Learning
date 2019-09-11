75ba72eea7950101fafea9582520f2b82819ed17
Rana53 <ranajahid.cseru@gmail.com>
2019-09-11 15:28:22 +0600

    basic js file interaction with html file done.

--- 01BasicWeb/script.js
+++ 01BasicWeb/script.js
@@ -1,3 +1,11 @@
-const myElement = document.querySelector('p');
+// // // const myElement = document.querySelector('p');
+// // // console.log(myElement);
+// // // myElement.textContent = 'I am changed using JS file'
+// // const myElement = document.querySelectorAll('p');
+// // const elementRemove = (element) => element.remove();
+// // myElement.forEach(element => elementRemove(element));
+// const myElement = document.title;
+// console.log(myElement);
+// console.log(document.getElementsByClassName('classOne'))
+const myElement = document.querySelectorAll('#idOne');
 console.log(myElement);
-myElement.textContent = 'I am changed using JS file'
