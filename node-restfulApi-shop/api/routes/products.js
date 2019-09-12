9a7e22247a3d1a9e2cbb0614cf8b90a8b05c9c11
Rana53 <ranajahid.cseru@gmail.com>
2019-09-12 19:59:50 +0600

    create products and orders route api

---
+++ node-restfulApi-shop/api/routes/products.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET request'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling post request'
+    });
+});
+
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: `Handling GET request wiht product id ${id}`
+    });
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: `Handling PATCH request wiht product id ${id}`
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: `Handling DELETE request wiht product id ${id}`
+    });
+});
+
+module.exports = router;
