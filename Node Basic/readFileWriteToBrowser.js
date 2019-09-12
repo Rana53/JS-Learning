c1045832d3a99dcd5de57081652a708c8f7c15c1
Rana53 <ranajahid.cseru@gmail.com>
2019-09-12 11:41:11 +0600

    send html to browser

--- Node Basic/readFileWriteToBrowser.js
+++ Node Basic/readFileWriteToBrowser.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    var readStream = fs.createReadStream('text.txt', 'utf8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var readStream = fs.createReadStream('index.html', 'utf8');
     readStream.pipe(res);
 }).listen(8080);
