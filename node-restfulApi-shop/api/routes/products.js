438071d8a9a0961538e01d1a4b71bdb9eba2eee2
Rana53 <ranajahid.cseru@gmail.com>
2019-09-14 16:55:20 +0600

    connect to mongoDB atlas database

--- node-restfulApi-shop/api/routes/products.js
+++ node-restfulApi-shop/api/routes/products.js
@@ -1,28 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request'
-    });
+  Product.find()
+  .exec()
+  .then(docs => {
+    console.log("All document are: \n" + docs);
+    res.status(200).json(docs);
+  })
+  .catch(err => {
+    res.status(500).json({error: err});
+  });
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    }
-    res.status(200).json({
-        message: 'Handling post request',
-        createProduct: product
+    const product = new Product({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      price: req.body.price
     });
+    product.save()
+    .then(result =>{
+      console.log(result)
+      res.status(200).json({
+          message: 'Handling post request /product',
+          createProduct: product
+      });
+    })
+    .catch(err =>{
+      console.log(err)
+      res.status(500).json({error: err});
+    });
+
 });
 
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: `Handling GET request wiht product id ${id}`
+    Product.findById(id)
+  //  .exec()
+    .then(doc => {
+      console.log("From database: " + doc);
+      if(doc)
+        res.status(200).json(doc);
+      else
+        res.status(404).json({message: "No valid data formate in this id"});
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(200).json({error: err});
     });
 });
 
--- node-restfulApi-shop/app.js
+++ node-restfulApi-shop/app.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const dbURL  = 'mongodb+srv://node-shop:node-shop@cluster0-y2tlm.mongodb.net/test?retryWrites=true&w=majority';
 
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+mongoose.connect(dbURL,{ useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connection.on('connected', function(){
+    console.log("Mongoose default connection is open to ", dbURL);
+ });
+ mongoose.connection.on('error', function(err){
+     console.log("Mongoose default connection has occured "+err+" error");
+});
+mongoose.connection.on('disconnected', function(){
+     console.log("Mongoose default connection is disconnected");
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
@@ -19,6 +31,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 })
+
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
