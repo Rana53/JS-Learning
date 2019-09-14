379b512383e2af098c3b6c9ca1a752c08a23d5a7
Rana53 <ranajahid.cseru@gmail.com>
2019-09-14 20:50:28 +0600

    product and order all process done with mongooes and mongodb

--- node-restfulApi-shop/api/routes/orders.js
+++ node-restfulApi-shop/api/routes/orders.js
@@ -1,34 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'orders was created'
+    Order.find()
+    .populate('product')
+    .select("_id quantity productId ")
+    .exec()
+    .then( docs =>{
+      if(docs){
+        res.status(200).json(docs);
+      }
+      else
+        console.log('No order found');
+    })
+    .catch( err => {
+      res.status(500).json({ error: err});
     });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(200).json({
-        message: 'Handling POST request in orders api',
-        order: order
+    const order = new Order({
+      _id: mongoose.Types.ObjectId(),
+      productId: req.body.productId,
+      quantity: req.body.quantity
+    });
+    Product.findById(req.body.productId)
+    .then(product =>{
+      if(!product)
+        return res.status(404).json({message: "product not found"});
+      order.save()
+      .then(result =>{
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err});
+      });
+    })
+    .catch(err => {
+      res.status(500).json({message: "id not found"});
     });
 });
 
 router.get('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
-    res.status(200).json({
-        message: `Handling GET request in orders api with id ${id}`
+    Order.findById({_id: id})
+    .exec()
+    .then( doc =>{
+      if(!doc)
+        return res.status(404).json({ message: "order not fount"});
+      res.status(200).json(doc);
+    })
+    .catch( err => {
+      res.status(500).json({ error: err});
     });
 });
 
 router.delete('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
-    res.status(200).json({
-        message: `Handling DELETE request in orders api with id ${id}`
+    Order.deleteOne({_id: id})
+    .exec()
+    .then( result => {
+      console.log('successfully delete with id '+ result._id);
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log("error in delete");
+      res.status(500).json({
+        error: err
+      })
     });
 });
 
--- node-restfulApi-shop/api/routes/products.js
+++ node-restfulApi-shop/api/routes/products.js
@@ -56,15 +56,33 @@ router.get('/:productId', (req, res, next) => {
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: `Handling PATCH request wiht product id ${id}`
+    const updateOps = {};
+    for(const ops of req.body){
+      updateOps[ops.propName] = ops.value
+    }
+    Product.update({_id: id},{ $set: updateOps})
+    .exec()
+    .then(result=>{
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err});
     });
 });
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: `Handling DELETE request wiht product id ${id}`
+    Product.deleteOne({_id: id})
+    .exec()
+    .then(result => {
+      console.log("successfully deleted");
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log("error in delete");
+      res.status(500).json({
+        error: err
+      })
     });
 });
 
