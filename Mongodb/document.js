1afaa1c62363e843edb0b41568347238250f281f
Rana53 <ranajahid.cseru@gmail.com>
2019-09-27 23:55:41 +0600

    validation checked

--- Mongodb/document.js
+++ Mongodb/document.js
@@ -6,10 +6,14 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console,'connection error:'));
 db.once('open', function(){
   console.log('we are connected');
+});
+const schema = new Schema({ name: String, age: { type: Number, min: 0 } });
+const Person = mongoose.model('Person', schema);
 
-const MyModel = mongoose.model('Test', new Schema({ name: String }));
-const doc = new MyModel();
+let p = new Person({ name: 'foo', age: 'bar' });
+// Cast to Number failed for value "bar" at path "age"
+await p.validate();
 
-var a = doc instanceof MyModel; // true
-var b = doc instanceof mongoose.Model; // true
-//var c = doc instanceof mongoose.Document; // true
+let p2 = new Person({ name: 'foo', age: -1 });
+// Path `age` (-1) is less than minimum allowed value (0).
+await p2.validate();
